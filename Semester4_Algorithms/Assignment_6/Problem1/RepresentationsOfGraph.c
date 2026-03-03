1792d301bd9a108f01baa2f46843be9c933d1428
KakarotDB <mrigajpersonal@gmail.com>
2026-03-03 10:01:05 +0530

    Did Assignment6/Problem1 - Adjacency List representation of graph with and without weights

---
+++ Semester4_Algorithms/Assignment_6/Problem1/RepresentationsOfGraph.c
@@ -0,0 +1,126 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct Node {
+    struct Node *next;
+    int vertex;
+    int weight;
+} Node;
+
+Node **createAdjacencyList(int vertices) {
+    Node **list = (Node **)malloc(vertices * sizeof(Node *));
+    for (int i = 0; i < vertices; i++) {
+        list[i] = NULL;
+    }
+    return list;
+}
+
+bool addEdgeAdjacencyListDirectedWithoutWeight(Node **list, int u, int v) {
+    if (list == NULL)
+        return false;
+
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    if (!newNode)
+        return false;
+
+    newNode->vertex = v;
+    newNode->weight = 0;
+    newNode->next = list[u];
+    list[u] = newNode;
+
+    return true;
+}
+
+bool addEdgeAdjacencyListUnDirectedWithoutWeight(Node **list, int u, int v) {
+    if (!addEdgeAdjacencyListDirectedWithoutWeight(list, u, v))
+        return false;
+    if (!addEdgeAdjacencyListDirectedWithoutWeight(list, v, u))
+        return false;
+    return true;
+}
+
+bool addEdgeAdjacencyListDirectedWithWeight(Node **list, int u, int v,
+                                            int weight) {
+    if (list == NULL)
+        return false;
+
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    if (!newNode)
+        return false;
+
+    newNode->vertex = v;
+    newNode->weight = weight;
+    newNode->next = list[u];
+    list[u] = newNode;
+
+    return true;
+}
+
+bool addEdgeAdjacencyListUnDirectedWithWeight(Node **list, int u, int v,
+                                              int weight) {
+    if (!addEdgeAdjacencyListDirectedWithWeight(list, u, v, weight))
+        return false;
+    if (!addEdgeAdjacencyListDirectedWithWeight(list, v, u, weight))
+        return false;
+    return true;
+}
+
+void displayAdjacencyList(Node **list, int vertices) {
+    printf("\nAdjacency List Representation\n");
+    printf("------------------------------\n");
+    for (int i = 0; i < vertices; i++) {
+        printf("Vertex [%d]: ", i);
+        Node *temp = list[i];
+
+        while (temp != NULL) {
+            printf("%d(w:%d) -> ", temp->vertex, temp->weight);
+            temp = temp->next;
+        }
+        printf("NULL\n");
+    }
+    printf("------------------------------------\n");
+}
+
+void freeAdjacencyList(Node **list, int vertices) {
+    if (list == NULL)
+        return;
+
+    for (int i = 0; i < vertices; i++) {
+        Node *current = list[i];
+        while (current != NULL) {
+            Node *temp = current;
+            current = current->next;
+            free(temp);
+        }
+    }
+    free(list);
+}
+
+int main() {
+    int n = 100; // number of vertices
+    // the vertices are [0, 99];
+    srand(time(NULL));
+    Node **list = createAdjacencyList(n);
+
+    for (int i = 0; i < n; i++) {
+        int u = rand() % 100;
+        int v = rand() % 100;
+        addEdgeAdjacencyListDirectedWithoutWeight(list, u, v);
+    }
+
+    displayAdjacencyList(list, n);
+
+    for (int i = 0; i < n; i++) {
+        int u = rand() % 100;
+        int v = rand() % 100;
+        int weight = rand() % 50 + 1;
+        addEdgeAdjacencyListDirectedWithWeight(list, u, v, weight);
+    }
+
+    displayAdjacencyList(list, n);
+
+    freeAdjacencyList(list, n);
+    return 0;
+}
