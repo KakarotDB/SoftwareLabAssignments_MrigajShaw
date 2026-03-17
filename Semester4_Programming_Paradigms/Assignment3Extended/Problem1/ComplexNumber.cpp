95be01f7d267f929a244634b1b449b7e57efb8a7
KakarotDB <mrigajpersonal@gmail.com>
2026-03-17 19:56:57 +0530

    [UPDATE] Programming Lab Assignment3Extended Problem1, Problem3

--- Semester4_Programming_Paradigms/Assignment3Extended/Problem1/ComplexNumber.cpp
+++ Semester4_Programming_Paradigms/Assignment3Extended/Problem1/ComplexNumber.cpp
@@ -1,55 +1,61 @@
 #include <iostream>
 
-namespace Complex{
-	class ComplexNumber {
-		int m_real;
-		int m_imaginary;
+namespace Complex {
+class ComplexNumber {
+    int m_real;
+    int m_imaginary;
 
-		public:
-			ComplexNumber(int real = 0, int imaginary = 0) : m_real(real), m_imaginary(imaginary) {}
+  public:
+    ComplexNumber(int real = 0, int imaginary = 0)
+        : m_real(real), m_imaginary(imaginary) {}
 
-			ComplexNumber(const ComplexNumber &other) {
-				m_real = other.m_real;
-				m_imaginary = other.m_imaginary;
-			}
+    ComplexNumber(const ComplexNumber &other) {
+        m_real = other.m_real;
+        m_imaginary = other.m_imaginary;
+    }
 
-			ComplexNumber operator=(const ComplexNumber &other) {
-				if(this != &other) {
-					m_real = other.m_real;
-					m_imaginary = other.m_imaginary;
-				}
-				return *this;
-			}
+    ComplexNumber operator=(const ComplexNumber &other) {
+        if (this != &other) {
+            m_real = other.m_real;
+            m_imaginary = other.m_imaginary;
+        }
+        return *this;
+    }
 
-			ComplexNumber operator+(const ComplexNumber &other) {
-				return ComplexNumber(m_real + other.m_real, m_imaginary + other.m_imaginary);
-			}
+    ComplexNumber operator+(const ComplexNumber &other) {
+        return ComplexNumber(m_real + other.m_real,
+                             m_imaginary + other.m_imaginary);
+    }
 
-			void display() const {
-				std::cout << m_real << "+" << m_imaginary << "i" << std::endl;
-			}
+    void display() const {
+        std::cout << m_real << "+" << m_imaginary << "i" << std::endl;
+    }
 
-			friend ComplexNumber operator-(const ComplexNumber &first, const ComplexNumber &second);
-	};
+    friend ComplexNumber operator-(const ComplexNumber &first,
+                                   const ComplexNumber &second);
+};
 
-	ComplexNumber operator-(const ComplexNumber &first, const ComplexNumber &second) {
-		return ComplexNumber(first.m_real - second.m_real, first.m_imaginary - second.m_imaginary);
-	}
+ComplexNumber operator-(const ComplexNumber &first,
+                        const ComplexNumber &second) {
+    return ComplexNumber(first.m_real - second.m_real,
+                         first.m_imaginary - second.m_imaginary);
 }
+} // namespace Complex
 
 int main() {
-	using namespace std;
-	using namespace Complex;
-	ComplexNumber c1(1,1);
-	ComplexNumber c2(2,2);
-	cout << "c1 : ";
-	c1.display();
-	cout << "c2 : ";
-	c2.display();
+    using namespace std;
+    using namespace Complex;
+    ComplexNumber c1(1, 1);
+    ComplexNumber c2(2, 2);
+    cout << "c1 : ";
+    c1.display();
+    cout << "c2 : ";
+    c2.display();
 
-	ComplexNumber c3;
-	c3 = c2 - (2, 3);
-	cout << "c3 : "; c3.display();
+    ComplexNumber c3;
+    c3 = c1 - c2;
+    cout << "c3 : ";
+    c3.display();
 
-	return 0;
+    return 0;
 }
--- Semester4_Programming_Paradigms/Assignment3Extended/Problem3/BankingSystem.cpp
+++ Semester4_Programming_Paradigms/Assignment3Extended/Problem3/BankingSystem.cpp
@@ -1,63 +1,67 @@
 #include <iostream>
 
 namespace BankingSystem {
-	class BankManager;
+class BankManager;
 
-	class BankAccount {
-		std::string m_name;
-		int m_accountNumber;
-		double m_currentBalance;
+class BankAccount {
+    std::string m_name;
+    int m_accountNumber;
+    double m_currentBalance;
 
-		public:
-			BankAccount(std::string name, int accountNumber, double currentBalance, BankManager &manager);
-			friend class BankManager;
-	};
+  public:
+    BankAccount(std::string name, int accountNumber, double currentBalance,
+                BankManager &manager);
+    friend class BankManager;
+};
 
-	class BankManager {
-		static int countAccounts;
-		static int sufficientAmount;
-		public:
-			void display(BankAccount account) const {
-				std::cout << "Name : " << account.m_name << std::endl
-					<< "Account Number : " << account.m_accountNumber << std::endl 
-					<< "Current Balance : " << account.m_currentBalance << std::endl;
-			}
-			void displayCountAccounts() const {
-				std::cout << "Count of accounts = " << countAccounts << std::endl;
-			}
-			void addAccount() {
-				countAccounts++;
-			}
+class BankManager {
+    static int countAccounts;
+    static int sufficientAmount;
 
-			bool hasSufficientBalance(BankAccount &account) {
-				return account.m_currentBalance >= sufficientAmount;
-			}
-	};
-	int BankManager::countAccounts = 0;
-	int BankManager::sufficientAmount = 10000;
+  public:
+    void display(BankAccount account) const {
+        std::cout << "Name : " << account.m_name << std::endl
+                  << "Account Number : " << account.m_accountNumber << std::endl
+                  << "Current Balance : " << account.m_currentBalance
+                  << std::endl;
+    }
+    void displayCountAccounts() const {
+        std::cout << "Count of accounts = " << countAccounts << std::endl;
+    }
+    void addAccount() { countAccounts++; }
 
-	BankAccount::BankAccount(std::string name, int accountNumber, double currentBalance, BankManager &manager): m_name(name), m_accountNumber(accountNumber) {
-		manager.addAccount();
-		if(currentBalance <= 0) m_currentBalance = 0;
-		else m_currentBalance = currentBalance;
-	}
+    bool hasSufficientBalance(BankAccount &account) {
+        return account.m_currentBalance >= sufficientAmount;
+    }
+};
+int BankManager::countAccounts = 0;
+int BankManager::sufficientAmount = 10000;
 
-
+BankAccount::BankAccount(std::string name, int accountNumber,
+                         double currentBalance, BankManager &manager)
+    : m_name(name), m_accountNumber(accountNumber) {
+    manager.addAccount();
+    if (currentBalance <= 0)
+        m_currentBalance = 0;
+    else
+        m_currentBalance = currentBalance;
 }
 
+} // namespace BankingSystem
+
 int main() {
-	using namespace std;
-	using namespace BankingSystem;
+    using namespace std;
+    using namespace BankingSystem;
 
-	BankManager manager;
-	BankAccount b("Mrigaj", 1, 1000, manager);
-	manager.display(b);
-	manager.displayCountAccounts();
+    BankManager manager;
+    BankAccount b("Mrigaj", 1, 1000, manager);
+    manager.display(b);
+    manager.displayCountAccounts();
 
-	BankAccount b1("Rahul", 2, 2000, manager);
-	manager.display(b1);
-	manager.displayCountAccounts();
-	cout << "Does bank account 1 have sufficient balance ?" << endl;
-	cout << boolalpha << manager.hasSufficientBalance(b) << endl;
-	return 0;
+    BankAccount b1("Rahul", 2, 2000, manager);
+    manager.display(b1);
+    manager.displayCountAccounts();
+    cout << "Does bank account 1 have sufficient balance ?" << endl;
+    cout << boolalpha << manager.hasSufficientBalance(b) << endl;
+    return 0;
 }
