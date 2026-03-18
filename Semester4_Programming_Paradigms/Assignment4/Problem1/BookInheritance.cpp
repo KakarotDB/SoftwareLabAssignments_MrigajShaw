30c8735e38d966ea07f701fa347394087a4eb046
KakarotDB <mrigajpersonal@gmail.com>
2026-03-18 21:59:59 +0530

    [IN PROGRESS] Assignment4/Problem1

--- Semester4_Programming_Paradigms/Assignment4/Problem1/BookInheritance.cpp
+++ Semester4_Programming_Paradigms/Assignment4/Problem1/BookInheritance.cpp
@@ -22,64 +22,67 @@ class Book {
         m_title = stringUtils::cloneString(title);
     }
 
-	Book(const Book &other): m_pages(other.m_pages) {
-		m_title = stringUtils::cloneString(other.m_title);
-	}
+    Book(const Book &other) : m_pages(other.m_pages) {
+        m_title = stringUtils::cloneString(other.m_title);
+    }
 
-	const Book &operator=(const Book &other) {
-		if(this == &other) return *this;
+    const Book &operator=(const Book &other) {
+        if (this == &other)
+            return *this;
 
-		delete[] m_title;
-		m_title = stringUtils::cloneString(other.m_title);
-		m_pages = other.m_pages;
-	}
+        delete[] m_title;
+        m_title = stringUtils::cloneString(other.m_title);
+        m_pages = other.m_pages;
 
-	virtual ~Book() {
-		delete[] m_title;
-		m_title = nullptr;
-	}
+        return *this;
+    }
 
-	virtual void readBookData() {
-		char buffer[256];
-		std::cout << "Enter title of book: ";
-		std::cin.getline(buffer, 256);
+    virtual ~Book() {
+        delete[] m_title;
+        m_title = nullptr;
+    }
 
-		delete[] m_title;
-		m_title = stringUtils::cloneString(buffer);
+    virtual void readBookData() {
+        char buffer[256];
+        std::cout << "Enter title of book: ";
+        std::cin.getline(buffer, 256);
 
-		std::cout << "Enter number of pages of book: ";
-		std::cin >> m_pages;
-	}
+        delete[] m_title;
+        m_title = stringUtils::cloneString(buffer);
 
-	virtual void displayBookData() {
-		std::cout << "Title: " << m_title << std::endl;
-		std::cout << "Pages: " << m_pages << std::endl;
-	}
+        std::cout << "Enter number of pages of book: ";
+        std::cin >> m_pages;
+    }
+
+    virtual void displayBookData() {
+        std::cout << "Title: " << m_title << std::endl;
+        std::cout << "Pages: " << m_pages << std::endl;
+    }
 };
 
-	class EBook : public Book {
-		double m_fileSizeMB;
-		char* m_format;
+class EBook : public Book {
+    double m_fileSizeMB;
+    char *m_format;
 
-		public:
+  public:
+    EBook(double fileSizeMB = 0.0, const char *format = "N\\A")
+        : m_fileSizeMB(fileSizeMB) {
+        m_format = stringUtils::cloneString(format);
+    }
 
-		EBook(double fileSizeMB = 0.0, const char* format = "N\\A"): m_fileSizeMB(fileSizeMB){
-			m_format = stringUtils::cloneString(format);
-		}
-
-		EBook(const EBook &other): Book(other), m_fileSizeMB(other.m_fileSizeMB) {
-			delete[] m_format;
-			m_format = stringUtils::cloneString(other.m_format);
-		}
-	};
+    EBook(const EBook &other) : Book(other), m_fileSizeMB(other.m_fileSizeMB) {
+        delete[] m_format;
+        m_format = stringUtils::cloneString(other.m_format);
+    }
+};
 } // namespace bookManagementSystem
 
 int main() {
-	using namespace bookManagementSystem;
-	Book b1;
-	b1.displayBookData();
-	b1.readBookData();
-	b1.displayBookData();
+    using namespace bookManagementSystem;
+    Book b1;
+    b1.displayBookData();
+    b1.readBookData();
+    b1.displayBookData();
 
-	EBook b2;
+    EBook b2;
 }
