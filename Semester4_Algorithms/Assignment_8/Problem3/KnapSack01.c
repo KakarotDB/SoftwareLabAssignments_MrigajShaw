9a2ead9436c2372685cff4cd47907cf47308c9ab
KakarotDB <mrigajpersonal@gmail.com>
2026-04-07 20:29:21 +0530

    [COMPLETED] Assignment8/problem3

--- Semester4_Algorithms/Assignment_8/Problem3/KnapSack01.c
+++ Semester4_Algorithms/Assignment_8/Problem3/KnapSack01.c
@@ -1,44 +1,85 @@
 #include <stdio.h>
-#define ll long long 
+#include <stdlib.h>
+#include <time.h>
+#define ll long long
+#define max(a, b) ((a) > (b) ? (a) : (b))
 
 ll maxProfitRecursive(ll values[], ll weights[], int n, int W) {
-	if(n == 0 || W == 0) return 0;
+    if (n == 0 || W == 0)
+        return 0;
 
-	if(weight[n - 1] > W) {
-		return maxProfitRecursive(values, weights, n - 1, W);
-	}
-	else {
-		return max(values[n - 1] + maxProfitRecursive(values, weights, n - 1, W - weights[n - 1]), maxProfitRecursive(values, weights, n - 1, W);
-	}
+    if (weights[n - 1] > W) {
+        return maxProfitRecursive(values, weights, n - 1, W);
+    } else {
+        ll take = values[n - 1] + maxProfitRecursive(values, weights, n - 1,
+                                                     W - weights[n - 1]);
+        ll leave = maxProfitRecursive(values, weights, n - 1, W);
+
+        return max(take, leave);
+    }
 }
 
 ll maxProfitDynamicProgramming(ll *values, ll *weights, int n, int W) {
-	ll dp[n + 1][W + 1];
+    ll dp[n + 1][W + 1];
 
-	int n_values, n_weights, KnapSack_Weight;
-	printf("Enter number of values and weights: ");
-	scanf("%d %d", &n, &n_weights);
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= W; j++) {
+            dp[i][j] = 0;
+        }
+    }
 
-	ll values[n_values], weights[n_weights];
+    int Wj = 0;
+    int Vi = 0;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= W; j++) {
+            Wj = weights[i - 1];
+            Vi = values[i - 1];
 
-	printf("Enter %d values: \n", n)h
-	for(int i = 0; i < n_values; i++) {
-		int vi = 0;
-		scanf("%d", &vi);
-		values[i] = vi;
-	}
+            if (Wj > j) {
+                dp[i][j] = dp[i - 1][j];
+            } else {
+                dp[i][j] = max(dp[i - 1][j], Vi + dp[i - 1][j - Wj]);
+            }
+        }
+    }
 
-	for(int i = 0; i <= W; i++) {
-		dp[0][i] = 0;
-	}
-	for(int i = 0; i <= n; i++) {
-		dp[i][0] = 0;
-	}
+    return dp[n][W];
+}
 
-	for(int i = 1; i <= n; i++) {
-		for(int j = 1; j<= W; j++) {
-			ll vi = values[i];
-			ll wi = weights[i];
-		}
-	}
+int main() {
+    ll n = 0;
+    ll KnapSack_Capacity = 0;
+    clock_t start, end;
+
+    printf("Enter number of items: ");
+    scanf("%lld", &n);
+
+    printf("Enter capacity of knapsack: ");
+    scanf("%lld", &KnapSack_Capacity);
+
+    ll values[n], weights[n];
+
+    printf("Enter %lld pairs (values, weights)\n", n);
+
+    for (ll i = 0; i < n; i++) {
+        scanf("%lld %lld", &values[i], &weights[i]);
+    }
+
+    printf("Bruteforce solution result : \n");
+    ll result = 0;
+    start = clock();
+    result = maxProfitRecursive(values, weights, n, KnapSack_Capacity);
+    end = clock();
+
+    printf("Result = %lld\n", result);
+    printf("Time taken = %.10f\n", (double)(end - start) / CLOCKS_PER_SEC);
+
+    printf("DP solution result : \n");
+    result = 0;
+    start = clock();
+    result = maxProfitDynamicProgramming(values, weights, n, KnapSack_Capacity);
+    end = clock();
+
+    printf("Result = %lld\n", result);
+    printf("Time taken = %.10f\n", (double)(end - start) / CLOCKS_PER_SEC);
 }
