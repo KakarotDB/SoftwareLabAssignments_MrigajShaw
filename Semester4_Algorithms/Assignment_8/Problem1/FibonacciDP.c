aa54a03bf0105f3be501ba7023ed5ca497766dae
KakarotDB <mrigajpersonal@gmail.com>
2026-04-07 19:39:01 +0530

    [COMPLETED] Assignment8/problem1

--- Semester4_Algorithms/Assignment_8/Problem1/FibonacciDP.c
+++ Semester4_Algorithms/Assignment_8/Problem1/FibonacciDP.c
@@ -6,65 +6,74 @@
 ll memo[MAX];
 
 ll fibonacciRecursion(ll n) {
-	if(n <= 0) return 0;
-	if(n == 1) return 1;
-	return fibonacciRecursion(n - 1) + fibonacciRecursion(n - 2);	
+    if (n <= 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    return fibonacciRecursion(n - 1) + fibonacciRecursion(n - 2);
 }
 
 ll fibonacciMemoization(int n) {
-	if(n <= 1) return n;
+    if (n <= 1)
+        return n;
 
-	if(memo[n] != -1) return memo[n];
+    if (memo[n] != -1)
+        return memo[n];
 
-	memo[n] = fibonacciMemoization(n - 1) + fibonacciMemoization(n - 2);
+    memo[n] = fibonacciMemoization(n - 1) + fibonacciMemoization(n - 2);
 
-	return memo[n];
+    return memo[n];
 }
 
 ll fibonacciTabulation(int n) {
-	ll dp[n + 1];
-	ll f0 = 0;
-	ll f1 = 1;	
-	ll f2 = 0;
+    ll dp[n + 1];
+    ll f0 = 0;
+    ll f1 = 1;
+    ll f2 = 0;
 
-	if(n == 0) return f0;
-	if(n == 1) return f1;
-	for(int i = 2; i <=n; i++) {
-		f2 = f0 + f1;
-		f0 = f1;
-		f1 = f2;
-	}
+    if (n == 0)
+        return f0;
+    if (n == 1)
+        return f1;
+    for (int i = 2; i <= n; i++) {
+        f2 = f0 + f1;
+        f0 = f1;
+        f1 = f2;
+    }
 
-	return f2; 
+    return f2;
 }
 
 ll main() {
-	//initializing global array to all -1 
-	for(int i = 0; i < MAX; i++) {
-		memo[i] = -1;
-	}
-	ll n = 0;
-	clock_t start, end;
+    // initializing global array to all -1
+    for (int i = 0; i < MAX; i++) {
+        memo[i] = -1;
+    }
+    ll n = 0;
+    clock_t start, end;
 
-	printf("Enter value of n: ");
-	scanf("%lld", &n);
-	
-	start = clock();
-	ll result = fibonacciRecursion(n);	
-	end = clock();
-	printf("%lld\n", result);
-	printf("%.10f\n", (double) (end - start)/CLOCKS_PER_SEC);
+    printf("Enter value of n: ");
+    scanf("%lld", &n);
 
-	start = clock();
-	result = fibonacciMemoization(n);
-	end = clock();
-	printf("%lld\n", result);
-	printf("%.10f\n", (double) (end - start)/CLOCKS_PER_SEC);
+    start = clock();
+    printf("Performing fibonacciRecursion: \n");
+    ll result = fibonacciRecursion(n);
+    end = clock();
+    printf("Result = %lld\n", result);
+    printf("Time taken = %.10f\n", (double)(end - start) / CLOCKS_PER_SEC);
 
-	start = clock();
-	result = fibonacciTabulation(n);
-	end = clock();
-	printf("%lld\n", result);
-	printf("%.10f\n", (double) (end - start)/CLOCKS_PER_SEC);
-	return 0;
+    printf("Performing fibonacciMemoization: \n");
+    start = clock();
+    result = fibonacciMemoization(n);
+    end = clock();
+    printf("Result = %lld\n", result);
+    printf("Time taken = %.10f\n", (double)(end - start) / CLOCKS_PER_SEC);
+
+    printf("Performing fibonacciTabulation: \n");
+    start = clock();
+    result = fibonacciTabulation(n);
+    end = clock();
+    printf("Result = %lld\n", result);
+    printf("Time taken = %.10f\n", (double)(end - start) / CLOCKS_PER_SEC);
+    return 0;
 }
