00f82c2e0befa93fdab7ff5617f20b82bfaea1da
MRIGAJ SHAW <mrigaj@hamsa.cs.iiests.ac.in>
2026-04-15 16:35:13 +0530

    [IN PROGRESS] Assignment4Extended/Problem3

--- Semester4_Programming_Paradigms/Assignment4Extended/Problem3/Cricketers.cpp
+++ Semester4_Programming_Paradigms/Assignment4Extended/Problem3/Cricketers.cpp
@@ -1,30 +1,45 @@
 #include <iostream>
 
 namespace CricketersManagementSystem {
+
+typedef Struct Match {
+	std::string opponentTeam;
+	int runsScored; //batting
+	int ballsPlayed; //batting
+	bool gotOut; //batting
+	int wicketsTaken; //bowling
+	double oversBowled; //bowling
+} Match;
+
 class Cricketer {
   protected:
     int m_matchesPlayed;
     std::string m_name;
-    std::string *m_opponents;
-
+	Match *m_matches;
   public:
     Cricketer(int matchesPlayed = 0, std::string name = "N/A",
-              const std::string *opponents = nullptr)
+              Match *matches = nullptr) 
         : m_matchesPlayed(matchesPlayed), m_name(name) {
-        if (m_matchesPlayed > 0 && opponents != nullptr) {
-            m_opponents = new std::string[m_matchesPlayed];
+        if (m_matchesPlayed > 0 && matches!= nullptr) {
+            m_matches = new Match[m_matchesPlayed];
 
             for (int i = 0; i < m_matchesPlayed; i++) {
-                m_opponents[i] = opponents[i];
+				m_matches[i] = matches[i];
             }
-        } else {
-            m_opponents = nullptr;
-        }
+        } 
     }
 
-    virtual ~Cricketer() { delete[] m_opponents; }
+    virtual ~Cricketer() { delete[] m_matches; }
 
     virtual void addRecord() = 0;
     virtual void displayStats() = 0;
+};
+
+class Batsman : virtual public Cricketer {
+	public:
+    	Batsman(int matchesPlayed = 0, std::string name = "N/A",
+              Match *matches = nullptr) : Cricketer(matchesPlayed, name, matches) {}
+		void addRecord()
+
 };
 } // namespace CricketersManagementSystem
