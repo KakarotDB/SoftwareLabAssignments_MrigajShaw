88a4cb2e29f8c29264b8eebaa170201f44ded637
KakarotDB <mrigajpersonal@gmail.com>
2026-07-29 16:33:09 +0530

    [UPDATE] DBMS Lab Assignment 1 Problem 1. Insertion File

---
+++ Semester5_DBMS/Assignment1/Problem1/TableInsertion.sql
@@ -0,0 +1,10 @@
+INSERT INTO
+  depts (deptcode, deptname)
+VALUES
+  ('CST', 'Computer Science & Technology'),
+  ('EE', 'ELectrical Engineering'),
+  ('ETC', 'Electronics Engineering'),
+  ('CE', 'Civil Engineering'),
+  ('IT', 'Information Technology'),
+  ('AEM', 'Aerospace & Applied Mechanics'),
+  ('MME', 'Materials and metallurgy');
